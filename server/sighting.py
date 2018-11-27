97dd45f14c87248d367756cd2a2844ceb2403fdb
Monica Tie <chibichiyi@gmail.com>
2018-11-27 13:18:26 -0800

    add ability to query for artist

--- server/sighting.py
+++ server/sighting.py
@@ -7,12 +7,26 @@ from server.db import get_db
 
 bp = Blueprint('sighting', __name__)
 
-@bp.route('/')
+@bp.route('/', methods=['GET', 'POST'])
 def index():
-    db = get_db()
-    sightings = db.execute(
-        'SELECT sighting_id, title, artist, date_created, museum_name, sighting_date, url, user_name'
-        ' FROM sightings'
-        ' LIMIT 10'
-    ).fetchall()
-    return render_template('sightings/index.html', sightings=sightings)
+    artist_sightings = None
+
+    if request.method == 'POST':
+        artist = request.form['artist']
+        db = get_db()
+        error = None
+
+        if artist:
+            artist_sightings = db.execute(
+                'SELECT sighting_id, title, artist, date_created, museum_name, sighting_date, url, user_name FROM sightings WHERE artist = ?', (artist,)
+            ).fetchall()
+
+            if artist_sightings:
+                return render_template('sightings/index.html', sightings=artist_sightings)
+            else:
+                error = 'No artists by the name of {}'.format(artist)
+
+        if error is not None:
+            flash(error)
+    return render_template('sightings/index.html')
+
