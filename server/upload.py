ee1d87d6952d100b2a823bedc9a1284f8b1bf541
Monica Tie <chibichiyi@gmail.com>
2019-01-04 11:58:02 -0800

    Generate and use a unique filename based on md5 hash and current timestamp

--- server/upload.py
+++ server/upload.py
@@ -1,9 +1,13 @@
+import time
+import hashlib
+
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for,
     current_app
 )
 from werkzeug.utils import secure_filename
-from aws import upload_file_to_s3
+from server.auth import login_required
+from server.aws import upload_file_to_s3
 
 bp = Blueprint('upload', __name__)
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif', 'mp4'])
@@ -17,6 +21,19 @@ def allowed_file(filename):
 def upload():
     return render_template('upload/upload.html')
 
+def generate_unique_filename(file):
+    return get_file_hash(file) + get_current_timestamp() + "." + file.filename.rsplit('.', 1)[1]
+
+def get_file_hash(file):
+    hasher = hashlib.md5()
+    buf = file.read()
+    hasher.update(buf)
+    return hasher.hexdigest()
+
+def get_current_timestamp():
+    millis = int(round(time.time() * 1000))
+    return str(millis)
+
 @bp.route('/upload', methods=['POST'])
 @login_required
 def upload_file():
@@ -28,9 +45,8 @@ def upload_file():
         return "Please select a file"
 
     if file and allowed_file(file.filename):
-        file.filename = secure_filename(file.filename)
+        file.filename = generate_unique_filename(file)
         output   	  = upload_file_to_s3(file, current_app.config["S3_BUCKET"])
         return str(output)
-
     else:
         return redirect("/upload")
